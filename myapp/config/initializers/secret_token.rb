4292ee9ff6b194af9b77a132984904d048caa2ac
tshibata <tshibata@tshibata-VirtualBox.(none)>
2012-12-21 22:18:13 +0900

    myappを作成しaddressスキャフォルドしました

---
+++ myapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'df3ad72477b9c02b1e5b5d998c5a83fdbc62eb9d131ff9e748b23940e70322fcff228606bdc0ec5b18004e33e85e89ee6a1792750f734867f4b05200f0dc7ba4'
